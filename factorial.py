799cdd7511eba3ff20e574193b8a768f67e7127d
Alicia Swain Henderson <126004938+ah1185@users.noreply.github.com>
2025-02-20 12:59:31 +0000

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -13,10 +13,12 @@ def endpoint():
     return "",400 # Bad Request
 
 def calculate(n):
-  if n <= 0:
-    return 1
-  else:
-    return n * calculate(n-1)
+    if n <= 0:
+        return 1
+    result = 1
+    for i in range(1, n+1):
+        result = i * result
+    return result
 
 if __name__ == "__main__":
   app.run(host="localhost",port=3000)
